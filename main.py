6881a97ddae9c97021dd01484521849dbf4c46eb
unknown <tamayo998@hotmail.com>
2020-10-06 20:36:19 -0500

    Data Preparation

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-#Cargamos los datos 
-data = pd.read_excel("titanic.xlsx",sheet_name=0) 
+#Cargamos los datos
+data = pd.read_excel("titanic.xlsx",sheet_name=0)
+data.head()
+data['Clase'] = data['Clase'].astype('category')
+data['Edad'] = data['Edad'].astype('category')
+data['Sexo'] = data['Sexo'].astype('category')
+data['Sobrevivio'] = data['Sobrevivio'].astype('category')
+data.info()
+data.describe()
+data['Sobrevivio'].value_counts().plot(kind='bar')
+dummiesClase = pd.get_dummies(data['Clase'])
+data = data.drop('Clase', axis=1)
+data = data.join(dummiesClase)
 data.head()
