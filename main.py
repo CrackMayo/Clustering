96db73216f9bffb49b27cdf333039b1db320465f
unknown <tamayo998@hotmail.com>
2020-10-06 21:04:56 -0500

    Model Learning and Evaluation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import metrics
 
 #Cargamos los datos
 data = pd.read_excel("titanic.xlsx",sheet_name=0)
@@ -16,3 +18,11 @@ dummiesClase = pd.get_dummies(data['Clase'])
 data = data.drop('Clase', axis=1)
 data = data.join(dummiesClase)
 data.head()
+
+#Aprendizaje y Evaluacion
+model = KMeans(n_clusters=5, max_iter=500)
+model.fit(data)
+print(f'inertia del modelo = {model.inertia_}')
+clusters= model.predict(data)
+sil=metrics.silhouette_score(data, clusters)
+print(f'Silueta= {sil}')
