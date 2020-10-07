f20d640dc6cfefec76f84372ee8dafc55253bfda
unknown <tamayo998@hotmail.com>
2020-10-06 21:56:15 -0500

    Profiling

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-#Cargamos los datos
 data = pd.read_excel("titanic.xlsx",sheet_name=0)
 data.head()
 data['Clase'] = data['Clase'].astype('category')
@@ -19,10 +18,28 @@ data = data.drop('Clase', axis=1)
 data = data.join(dummiesClase)
 data.head()
 
-#Aprendizaje y Evaluacion
 model = KMeans(n_clusters=5, max_iter=500)
 model.fit(data)
 print(f'inertia del modelo = {model.inertia_}')
 clusters= model.predict(data)
 sil=metrics.silhouette_score(data, clusters)
 print(f'Silueta= {sil}')
+
+centroides=pd.DataFrame(model.cluster_centers_, columns=data.columns.values)
+centroides.round(0)
+data['cluster']=model.predict(data)
+data.head()
+pd.value_counts(data["cluster"])
+
+def elbow():
+    ks = range(1, 20)
+    inertias = []
+    for k in ks:
+        model = KMeans(n_clusters=k)
+        model.fit(data)
+        inertias.append(model.inertia_)
+    plt.plot(ks, inertias, '-o')
+    plt.xlabel('Clusters num, k')
+    plt.ylabel('inertia')
+    plt.xticks(ks)
+    plt.show()
